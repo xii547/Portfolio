da452771ca06bfae1692670730d33f3e057425c2
hasan <hasanxiishahriar@gmail.com>
2025-04-09 21:52:29 +0600

    portfolio data added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -18,4 +19,26 @@ with col2:
     mollit anim id est laborum.
     """
 
-    st.write(content)
+    st.info(content)
+
+content2 = """
+Below is my portfolio where you can find some of the apps I've built in Python. Feel free to contact.
+"""
+
+st.write(content2)
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("006 data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("006 images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.image("006 images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
